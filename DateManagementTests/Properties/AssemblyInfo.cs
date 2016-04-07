da020d78e3c925d763c544b203313d08711c30a5
Antoine Barruol <abarruol@gmail.com>
2016-04-07 15:38:44 +0200

    Cleanup in the AssemblyInfos

--- DateManagementTests/Properties/AssemblyInfo.cs
+++ DateManagementTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DateManagementTests")]
+[assembly: AssemblyTitle("Working Days Management Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DateManagementTests")]
+[assembly: AssemblyProduct("Working Days Management Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
